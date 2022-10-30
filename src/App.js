e46ef5e6d59773f151e031b9916d7faf4b41938f
0xpr0f <94965754+0xPr0f@users.noreply.github.com>
2022-10-30 13:35:44 +0100

    edits

--- src/App.js
+++ src/App.js
@@ -154,6 +154,7 @@ function App() {
               <Route path="/swap" element={<Swap />} />
               <Route path="/vault" element={<Vault />} />
               <Route path="/bridge" element={<Bridge />} />
+                <Route path="/" exact element={<Faucet />} />
               <Route path="/faucet" element={<Faucet />} />
             </Routes>
           </div>
